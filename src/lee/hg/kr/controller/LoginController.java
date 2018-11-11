e687739d302c95ef7dcc9d16fdb64d5a53110327
leehg <dev@uffda.kr>
2018-11-11 12:42:00 +0900

    login button

--- src/lee/hg/kr/controller/LoginController.java
+++ src/lee/hg/kr/controller/LoginController.java
@@ -1,5 +1,12 @@
 package lee.hg.kr.controller;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -24,5 +31,27 @@ public class LoginController {
 	
 	public void setMainApp(Main main) {
 		this.main = main;
+	}
+	
+	@FXML
+	private void handleLoginButton() {
+//		System.out.println("loginbutton click~!");
+		String loginUrl = "http://game.bacoder.kr/login.jsp?login=suhan&pwd=1111";
+		try (Scanner scanner = new Scanner(new URL(loginUrl).openStream(), "UTF-8")){
+//			scanner.useDelimiter("\\A");
+			String result = scanner.next();
+//			System.out.println(result);
+			JSONObject json = new JSONObject(result);
+			System.out.println(json.toString());
+			if(json.getInt("id") > 0) {
+				System.out.println("success");
+			}else {
+				System.out.println("fail");
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
